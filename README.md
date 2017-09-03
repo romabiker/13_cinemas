d58cf91af6bdcbb5809d55d010302cf78e4f3020
Valery Ramusik <valer2677.@gmailcom>
2017-09-04 01:54:37 +0300

    Implement project

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
 # Cinemas
 
-[TODO. There will be project description]
+Sometimes you want to know the coolest films that are going on at the moment, but when you go to the [afisha](https://www.afisha.ru/msk/schedule_cinema/) it is difficult to determine which of them has a higher rating at [kinopoisk](https://www.kinopoisk.ru)
+
+The script displays to the console the most popular films currently being watched, sorted by rating.
+
+
+## Quickstart
+
+Example of script launch on Linux, Python 3.5:
+
+```
+$ pip install -r requirements.txt # alternatively try pip3
+
+$ python3 cinemas.py -h
+usage: cinemas.py [-h] [-p POP_LEVEL] [-c COUNT]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p POP_LEVEL, --pop_level POP_LEVEL
+                        min number of cinemas where film is on
+  -c COUNT, --count COUNT
+                        number of top rated movies
+
+$ python3 cinemas.py 
+
+Movie                                       Kinopoisk rating
+
+Твое имя                                  | 8.3
+Терминатор-2: Судный день                 | 8.3
+Квест                                     | 7.7
+Малыш на драйве                           | 7.6
+Мистер Штайн идет в онлайн                | 7.5
+Телохранитель киллера                     | 7.0
+Про любовь. Только для взрослых           | 6.9
+Валериан и город тысячи планет            | 6.8
+Тайна 7 сестер                            | 6.6
+Тюльпанная лихорадка                      | 6.5
+
+```
 
 # Project Goals
 
--- cinemas.py
+++ cinemas.py
@@ -1,20 +1,149 @@
+from collections import Counter
+import argparse
+import random
+import sys
 
 
-def fetch_afisha_page():
-    pass
+from bs4 import BeautifulSoup
+import requests
 
 
-def parse_afisha_list(raw_html):
-    pass
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+                        '-p', '--pop_level', type=int, default=10,
+                        help='min number of cinemas where film is on'
+                        )
+    parser.add_argument(
+                        '-c', '--count', default=10, type=int,
+                        help='number of top rated movies'
+                        )
+    return parser
 
 
-def fetch_movie_info(movie_title):
-    pass
+def count_cinemas(in_div):
+    return len(in_div.find_next('table').find_all('tr'))
 
 
-def output_movies_to_console(movies):
-    pass
+def compose_proxy_url(from_ip):
+    if from_ip:
+        return {
+                'http': 'http://{}'.format(from_ip),
+                }
+
+
+def extract_rating(from_html):
+    soup = BeautifulSoup(from_html, 'html.parser')
+    ratings_tag = soup.find('div', class_='element most_wanted')
+    if ratings_tag:
+        ratings_tag = ratings_tag.find('div', class_='rating')
+    if ratings_tag:
+        return ratings_tag.text
+
+
+def fetch_afisha_page(
+        afisha_cinema_url='https://www.afisha.ru/msk/schedule_cinema/'
+        ):
+    response = requests.get(afisha_cinema_url)
+    if not response.ok:
+        response.raise_for_status()
+    return response.text
+
+
+def fetch_films_info(film_titles, from_url='https://www.kinopoisk.ru'):
+    from_proxy_ips = fetch_proxy_ip_list()
+    from_user_agents = make_useragents_list()
+    films_with_ratings = Counter()
+    for title in film_titles:
+        search_params = produce_params(title)
+        rnd_proxy_url = compose_proxy_url(get_random(from_proxy_ips))
+        rnd_header = produce_headers(get_random(from_user_agents))
+        response = requests.get(
+                    from_url,
+                    params=search_params,
+                    headers=rnd_header,
+                    proxies=rnd_proxy_url,
+        )
+        if response.ok:
+            rating = extract_rating(response.text)
+        if rating:
+            films_with_ratings[title] = rating
+    return films_with_ratings
+
+
+def find_film_tags(in_soup):
+    return in_soup.find_all('div', class_='m-disp-table')
+
+
+def find_title(in_div):
+    return in_div.a.text
+
+
+def make_useragents_list(from_file='user_agents.txt'):
+    with open(from_file, 'r') as file_handler:
+        return file_handler.read().splitlines()
+
+
+def fetch_proxy_ip_list(
+                        proxy_provider='http://www.freeproxy-list.ru/\
+                        api/proxy?anonymity=false&token=demo'
+                        ):
+    response = requests.get(proxy_provider)
+    if response.ok:
+        return response.text.splitlines()
+
+
+def get_random(from_iterable):
+    if from_iterable:
+        return random.choice(from_iterable)
+
+
+def obtain_args():
+    parser = create_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    return namespace
+
+
+def produce_params(kp_query):
+    return {
+            'first': 'no',
+            'what': '',
+            'kp_query': kp_query
+           }
+
+
+def produce_headers(user_agent):
+    return {
+            'User-Agent': user_agent
+            }
+
+
+def is_popular_by(cinemas_num, pop_level):
+    return cinemas_num > pop_level
+
+
+def parse_afisha_list(raw_html, pop_level):
+    afisha_soup = BeautifulSoup(raw_html, 'html.parser')
+    popular_films = []
+    film_tags = find_film_tags(afisha_soup)
+    for in_div in film_tags:
+        film_title = find_title(in_div)
+        cinemas_number = count_cinemas(in_div)
+        if is_popular_by(cinemas_number, pop_level):
+            popular_films.append(film_title)
+    return popular_films
+
+
+def output_movies_to_console(movies, count):
+    print('\n{:<40}    {}\n'.format('Movie', 'Kinopoisk rating'))
+    for movie, rating in movies.most_common(count):
+        print('{:<40}  | {}'.format(movie, rating))
+    print()
 
 
 if __name__ == '__main__':
-    pass
+    args = obtain_args()
+    afisha_page = fetch_afisha_page()
+    popular_films = parse_afisha_list(afisha_page, args.pop_level)
+    films_kinopoisk_ratings = fetch_films_info(popular_films)
+    output_movies_to_console(films_kinopoisk_ratings, args.count)
